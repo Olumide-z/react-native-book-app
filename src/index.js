ca3a443e000a352d1d925d68eca36adc19b21f38
Olumide llasanmi <Olumide.llasanmi@ss-limited.com>
2025-11-28 20:24:02 +0100

    feat: cron added

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import cors from 'cors';
+import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
-import bookRoutes from './routes/bookRoutes.js';
-import { connectDB } from './lib/db.js';
+import bookRoutes from "./routes/bookRoutes.js";
+import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/books", bookRoutes)
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-    connectDB();
+  console.log(`server is running on port ${PORT}`);
+  connectDB();
 });
-
